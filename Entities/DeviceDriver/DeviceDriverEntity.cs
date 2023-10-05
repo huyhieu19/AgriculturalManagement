907c467dd0af8ec51d61c222c7b48f5aba4a630d
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-05 23:17:42 +0700

    Update table

--- Entities/DeviceDriver/DeviceDriverEntity.cs
+++ Entities/DeviceDriver/DeviceDriverEntity.cs
@@ -22,5 +22,6 @@ namespace Entities
         public ICollection<MachineWarranlyDateEntity>? MachineWarranlyDates { get; set; }
         public ICollection<ImageEntity>? Images { get; set; }
         public ICollection<TimerDeviceDriverEntity>? TimerDevices { get; set; }
+        public ICollection<DeviceInstrumentOnOffEntity>? DeviceInstrumentOnOffs { get; set; }
     }
 }
