64be7167fb11d9bd8c9da190816b68ba06601c31
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-09 23:38:32 +0700

    [DATN] Add mongo db

---
+++ Service/BackgroundJob/UploadInstrumentValueToMongoDbService.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Microsoft.Extensions.Hosting;
+using Service.Contracts;
+
+namespace Service
+{
+    public class UploadInstrumentValueToMongoDbService : BackgroundService
+    {
+        private readonly IDataStatisticsService dataStatisticsService;
+
+        public UploadInstrumentValueToMongoDbService(IDataStatisticsService dataStatisticsService)
+        {
+            this.dataStatisticsService = dataStatisticsService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Run every 5 seconds
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                await dataStatisticsService.PushDataToDB(new InstrumentValueByFiveSecondEntity()
+                {
+                    PayLoad = $"abc {DateTime.Now}",
+                    Topic = $"{new Random().NextInt64(1, 100)}",
+                    ValueDate = DateTime.Now,
+                });
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+        }
+    }
+}
---
+++ Service/DataStatistics/DataStatisticsService.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Microsoft.Extensions.Options;
+using Models;
+using MongoDB.Driver;
+using Service.Contracts;
+
+namespace Service
+{
+    public class DataStatisticsService : IDataStatisticsService
+    {
+        private readonly IMongoCollection<InstrumentValueByFiveSecondEntity> instrumentValue;
+        private readonly MongoClient client;
+        private readonly ILoggerManager logger;
+
+        public DataStatisticsService(IOptions<MongoDbConfig> mongoDbConfig, ILoggerManager logger)
+        {
+            this.client = new MongoClient(mongoDbConfig.Value.ConnectionString);
+            var database = this.client.GetDatabase(mongoDbConfig.Value.DatabaseName);
+            instrumentValue = database.GetCollection<InstrumentValueByFiveSecondEntity>(mongoDbConfig.Value.CollectionName);
+            this.logger = logger;
+        }
+
+        public async Task PushDataToDB(InstrumentValueByFiveSecondEntity addModel)
+        {
+            logger.LogInfomation("Push start");
+            await instrumentValue.InsertOneAsync(addModel);
+            logger.LogInfomation("Push end");
+        }
+    }
+}
